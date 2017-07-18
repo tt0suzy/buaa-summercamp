5983f974a1445364c077ec9ebd0ef67b24b7d680
tt0suzy <tt0suzy@gmail.com>
2017-07-19 00:18:20 +0800

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,9 @@
+def bubbleSort(array):
+    for i in range(len(array)-1):
+        for j in range(len(array)-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+array = [5,22,4,61,8,23,111]
+
+print bubbleSort(array)
